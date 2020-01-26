d53674b1af3d44be818da013b73480b5a48fadb1
PumpkinSeed <qwer.kocka@gmail.com>
2020-01-26 11:32:51 +0100

    Add new topics

--- kcs.go
+++ kcs.go
@@ -46,9 +46,9 @@ type Category struct {
 }
 
 func (c *Category) Print(last bool) {
-	_, _ = categoryHiColor.Println(charMultiplier(len(c.Name), "-"))
+	//_, _ = categoryHiColor.Println(charMultiplier(len(c.Name), "-"))
 	_, _ = categoryHiColor.Println(c.Name)
-	_, _ = categoryHiColor.Println(charMultiplier(len(c.Name), "-"))
+	_, _ = categoryColor.Println(charMultiplier(len(c.Name), "="))
 	if verbose {
 		_, _ = categoryColor.Println(c.Description)
 	}
@@ -60,7 +60,7 @@ func (c *Category) Print(last bool) {
 		width, _, _ := terminal.GetSize(0)
 		_, _ = categoryColor.Print("\n")
 		_, _ = categoryColor.Println(charMultiplier(width, "="))
-		_, _ = categoryColor.Print("\n")
+		//_, _ = categoryColor.Print("\n")
 	}
 }
 
@@ -203,9 +203,231 @@ var Instance = CheatSheet{
 					Command: "kubectl get pods --all-namespaces",
 					Description: "list all pods in all namespaces",
 				},
+				"pods-wide": {
+					Command: "kubectl get pods -o wide",
+					Description: "list all pods in the namespace, with more details",
+				},
+				"deployment": {
+					Command: "kubectl get deployment my-dep",
+					Description: "list a particular deployment",
+				},
+				"pods": {
+					Command: "kubectl get pods",
+					Description: "list all pods in the namespace",
+				},
+				"pod-yaml": {
+					Command: "kubectl get pod my-pod -o yaml ",
+					Description: "get a pod's YAML",
+				},
+				"pod-yaml-export": {
+					Command: "kubectl get pod my-pod -o yaml --export",
+					Description: "get a pod's YAML without cluster specific information",
+				},
+				"describe-nodes": {
+					Command: "kubectl describe nodes my-node",
+					Description: "describe commands with verbose output for nodes",
+				},
+				"describe-pods": {
+					Command: "kubectl describe pods my-pod",
+					Description: "describe commands with verbose output for pods",
+				},
 				"services-sort": {
 					Command: "kubectl get services --sort-by=.metadata.name",
 					Description: "list Services Sorted by Name",
+				},
+				"pods-sort": {
+					Command: "kubectl get pods --sort-by='.status.containerStatuses[0].restartCount'",
+					Description: "list pods Sorted by Restart Count",
+				},
+				"pv-sort": {
+					Command: "kubectl get pv -n test --sort-by=.spec.capacity.storage",
+					Description: "list PersistentVolumes in test namespace sorted by capacity",
+				},
+				"pods-selector": {
+					Command: "kubectl get pods --selector=app=cassandra -o jsonpath='{.items[*].metadata.labels.version}'",
+					Description: "get the version label of all pods with label app=cassandra",
+				},
+				"node-selector": {
+					Command: "kubectl get node --selector='!node-role.kubernetes.io/master'",
+					Description: "get all worker nodes (use a selector to exclude results that have a label named 'node-role.kubernetes.io/master')",
+				},
+				"pods-field-selector": {
+					Command: "kubectl get pods --field-selector=status.phase=Running",
+					Description: "get all running pods in the namespace",
+				},
+				"nodes-external-ip": {
+					Command: `kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="ExternalIP")].address}'`,
+					Description: "get ExternalIPs of all nodes",
+				},
+				"pods-rc": {
+					Command: `sel=${$(kubectl get rc my-rc --output=json | jq -j '.spec.selector | to_entries | .[] | "\(.key)=\(.value),"')%?} && echo $(kubectl get pods --selector=$sel --output=jsonpath={.items..metadata.name})`,
+					Description: "list Names of Pods that belong to Particular RC",
+				},
+				"pods-labels": {
+					Command: "kubectl get pods --show-labels",
+					Description: "show labels for all pods (or any other Kubernetes object that supports labelling)",
+				},
+				"nodes-ready": {
+					Command: `JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}' && kubectl get nodes -o jsonpath="$JSONPATH" | grep "Ready=True"`,
+					Description: "check which nodes are ready",
+				},
+				"pod-secrets": {
+					Command: "kubectl get pods -o json | jq '.items[].spec.containers[].env[]?.valueFrom.secretKeyRef.name' | grep -v null | sort | uniq",
+					Description: "list all Secrets currently in use by a pod",
+				},
+				"events": {
+					Command: "kubectl get events --sort-by=.metadata.creationTimestamp",
+					Description: "list Events sorted by timestamp",
+				},
+				"diff": {
+					Command: "kubectl diff -f ./my-manifest.yaml",
+					Description: "compares the current state of the cluster against the state that the cluster would be in if the manifest was applied",
+				},
+			},
+		},
+		"update": {
+			Name: "Updating Resources",
+			Commands: map[string]CommandDescriptor{
+				"set": {
+					Command: "kubectl set image deployment/frontend www=image:v2",
+					Description: `rolling update "www" containers of "frontend" deployment, updating the image`,
+				},
+				"rollout": {
+					Command: "kubectl rollout",
+					Args: []ArgumentDescriptor{
+						{
+							Argument:    "history deployment/frontend",
+							Description: "check the history of deployments including the revision",
+						},
+						{
+							Argument:    "undo deployment/frontend",
+							Description: "rollback to the previous deployment",
+						},
+						{
+							Argument:    "undo deployment/frontend --to-revision=2",
+							Description: "rollback to a specific revision",
+						},
+						{
+							Argument:    "status -w deployment/frontend",
+							Description: `watch rolling update status of "frontend" deployment until completion`,
+						},
+						{
+							Argument:    "restart deployment/frontend",
+							Description: `rolling restart of the "frontend" deployment`,
+						},
+					},
+				},
+				"replace-json": {
+					Command: "cat pod.json | kubectl replace -f -",
+					Description: "replace a pod based on the JSON passed into std",
+				},
+				"replace-force": {
+					Command: "kubectl replace --force -f ./pod.json",
+					Description: "force replace, delete and then re-create the resource. Will cause a service outage",
+				},
+				"expose": {
+					Command: "kubectl expose rc nginx --port=80 --target-port=8000",
+					Description: "create a service for a replicated nginx, which serves on port 80 and connects to the containers on port 8000",
+				},
+				"pods-image": {
+					Command: `kubectl get pod mypod -o yaml | sed 's/\(image: myimage\):.*$/\1:v4/' | kubectl replace -f -`,
+					Description: "update a single-container pod's image version (tag) to v4",
+				},
+				"label": {
+					Command: "kubectl label pods my-pod new-label=awesome",
+					Description: "add a Label",
+				},
+				"annotate": {
+					Command: "kubectl annotate pods my-pod icon-url=http://goo.gl/XXBTWq",
+					Description: "add an annotation",
+				},
+				"autoscale": {
+					Command: "kubectl autoscale deployment foo --min=2 --max=10",
+					Description: `auto scale a deployment "foo"`,
+				},
+			},
+		},
+		"patch": {
+			Name: "Patching Resources",
+			Commands: map[string]CommandDescriptor{
+				"node": {
+					Command: `kubectl patch node k8s-node-1 -p '{"spec":{"unschedulable":true}}'`,
+					Description: "partially update a node",
+				},
+				"pod": {
+					Command: `kubectl patch pod valid-pod -p '{"spec":{"containers":[{"name":"kubernetes-serve-hostname","image":"new image"}]}}'`,
+					Description: "update a container's image; spec.containers[*].name is required because it's a merge key",
+				},
+				"pod-array": {
+					Command: `kubectl patch pod valid-pod --type='json' -p='[{"op": "replace", "path": "/spec/containers/0/image", "value":"new image"}]'`,
+					Description: "update a container's image using a json patch with positional arrays",
+				},
+				"deployment": {
+					Command: `kubectl patch deployment valid-deployment  --type json   -p='[{"op": "remove", "path": "/spec/template/spec/containers/0/livenessProbe"}]'`,
+					Description: "disable a deployment livenessProbe using a json patch with positional arrays",
+				},
+				"sa": {
+					Command: `kubectl patch sa default --type='json' -p='[{"op": "add", "path": "/secrets/1", "value": {"name": "whatever" } }]'`,
+					Description: "add a new element to a positional array",
+				},
+			},
+		},
+		"edit": {
+			Name: "Editing Resources",
+			Commands: map[string]CommandDescriptor{
+				"default": {
+					Command: "kubectl edit svc/docker-registry",
+					Description: "edit the service named docker-registry",
+				},
+				"nano": {
+					Command: `KUBE_EDITOR="nano" kubectl edit svc/docker-registry`,
+					Description: "use an alternative editor",
+				},
+			},
+		},
+		"scale": {
+			Name: "Scaling Resources",
+			Commands: map[string]CommandDescriptor{
+				"replicas": {
+					Command: `kubectl scale --replicas=3 rs/foo`,
+					Description: `scale a replicaset named 'foo' to 3`,
+				},
+				"replicas-specific": {
+					Command: `kubectl scale --replicas=3 -f foo.yaml`,
+					Description: `scale a resource specified in "foo.yaml" to 3`,
+				},
+				"replicas-condition": {
+					Command: `kubectl scale --current-replicas=2 --replicas=3 deployment/mysql`,
+					Description: `if the deployment named mysql's current size is 2, scale mysql to 3`,
+				},
+				"replicas-multiple": {
+					Command: `kubectl scale --replicas=5 rc/foo rc/bar rc/baz`,
+					Description: `scale multiple replication controllers`,
+				},
+			},
+		},
+		"delete": {
+			Name: "Deleting Resources",
+			Commands: map[string]CommandDescriptor{
+				"file": {
+					Command: `kubectl delete -f ./pod.json`,
+					Description: `delete a pod using the type and name specified in pod.json`,
+				},
+				"name": {
+					Command: `kubectl delete pod,service baz foo`,
+					Description: `delete pods and services with same names "baz" and "foo"`,
+				},
+				"label": {
+					Command: `kubectl delete pods,services -l name=myLabel`,
+					Description: `delete pods and services with label name=myLabel`,
+				},
+				"namespace": {
+					Command: `kubectl -n my-ns delete pod,svc --all`,
+					Description: `delete all pods and services in namespace my-ns`,
+				},
+				"pattern": {
+					Command: `kubectl get pods  -n mynamespace --no-headers=true | awk '/pattern1|pattern2/{print $1}' | xargs  kubectl delete -n mynamespace pod`,
+					Description: `delete all pods matching the awk pattern1 or pattern2`,
 				},
 			},
 		},
